1001c8b63419766bce1d9af5fcb7a7cbbd95f4a5
jessica-luo <jessicaluoaustin@gmail.com>
2021-12-30 01:16:55 -0600

    limit to 280 char tweets, fixed song names

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import lyricsgenius
 import tweepy
 import os
 import re
+import textwrap
 
 api_keys = {
     'CONSUMER_KEY': os.getenv('CONSUMER_KEY'),
@@ -18,20 +19,36 @@ songs = list(map(lambda s: s.strip(), read_songs))
 
 def get_song_lyrics():
     genius = lyricsgenius.Genius(api_keys["GENIUS_TOKEN"])
-    song = random.choice(songs)
-    lyrics = genius.search_song(song, "BROCKHAMPTON").lyrics.replace("EmbedShare URLCopyEmbedCopy", "")
-    lines = lyrics.split('\n')
+
+    # choose random song from songs.txt
+    rand_song = random.choice(songs)
+
+    # remove punctuation from song
+    remove_slashes = rand_song.replace('/', ' ')
+    remove_extra_spaces = ' '.join(remove_slashes.split())
+    song = re.sub(r'[^\w\s]', '', remove_extra_spaces).replace(' ', '-')
+
+    # turn song into url 
+    lyrics = genius.lyrics(None, 'https://genius.com/Brockhampton-' + song + '-lyrics', True)
+
+    # remove footer
+    lyrics_replace_footer = lyrics.replace("EmbedShare URLCopyEmbedCopy", "")
+    lines = lyrics_replace_footer.split('\n')
     lines[-1] = re.sub('[0-9]+', '', lines[-1])
 
+    # remove empty lines
     filtered_empty_lines = filter(lambda line: line != "", lines)
-    filtered_verse_titles = filter(lambda line: "[" not in line, filtered_empty_lines)
-    filtered_lines = list(filtered_verse_titles)
 
+    # break lines at 140 characters
+    shorten_lines = map(lambda line: textwrap.fill(line, width=140, break_long_words=False), filtered_empty_lines)
+    filtered_lines = "\n".join(list(shorten_lines)).split("\n")
+
+    # choose 2 random lines to tweet
     random_num = random.randrange(0, len(filtered_lines)-1)
     tweetable_lyric = filtered_lines[random_num] + "\n" + filtered_lines[random_num+1]
     tweetable_lyric = tweetable_lyric.replace("\\", "")
 
-    return tweetable_lyric, song
+    return tweetable_lyric, rand_song
 
 def main():
     auth = tweepy.OAuthHandler(
